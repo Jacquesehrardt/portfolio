2e6cd5c9378a30473ce6570b927f01f04ca72975
Jacques Ehrardt <ehrardt38@gmail.com>
2023-06-30 19:07:35 -0700

    Header: add greeting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,45 @@
 import React from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faX, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+import NavList from "./NavList";
 
 class Header extends React.Component{
    render(){
       return(
          <header>
             <section>
-               <h1>
+               <h2>
                   <span>&#x7b;JAC</span>
                   /DEV&#x7d;
-               </h1>
+               </h2>
+               <nav>
+                  <NavList />
+                  <details>
+                     <summary>
+                        <FontAwesomeIcon icon={faBars} />
+                     </summary>
+                     <summary>
+                        <FontAwesomeIcon icon={faX} />
+                     </summary>
+                     <NavList />
+                  </details>
+               </nav>
             </section>
-            <nav>
-               <details>
-                  <summary>
-                     <FontAwesomeIcon icon={faBars} />
-                  </summary>
-                  <summary>
-                     <FontAwesomeIcon icon={faX} />
-                  </summary>
-               </details>
-            </nav>
+            <section>
+               <h1>
+                  Hello, I’m Jacques an
+                  <span>Web Developer</span>
+               </h1>
+               <h3>
+                  These are some of my works on this journey as a web developer.
+               </h3>
+               <a href="#skills">
+                  <FontAwesomeIcon icon={faChevronDown} />
+                  Click to scroll
+               </a>
+            </section>
          </header>
       )
    }
